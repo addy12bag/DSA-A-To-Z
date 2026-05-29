38227ace719776213869fddf45a3bb03a26ae03e
Sayantan Bag <sayantanbag24@gmail.com>
2026-05-29 12:18:44 +0530

    --__--

---
+++ Array/Medium/Add Two Numbers.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 + carry
+            carry = total // 10
+
+            current.next = ListNode(total % 10)
+            current = current.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
+    
+if __name__ == "__main__":
+    l1 = ListNode(2)
+    l1.next = ListNode(4)
+    l1.next.next = ListNode(3)
+
+    l2 = ListNode(5)
+    l2.next = ListNode(6)
+    l2.next.next = ListNode(4)
+
+    result = Solution().addTwoNumbers(l1, l2)
+
+    while result:
+        print(result.val)
+        result = result.next
